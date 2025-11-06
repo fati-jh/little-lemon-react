151dd0041c82bcbfe167d2e1c4cd3d876dd73c34
fati-jh <f.eljaouhari2019@gmail.com>
2025-11-06 01:15:13 +0100

    Add meta tags and open Graph setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import  Header  from './components/Header';
+import Main from './components/Main';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      little lemon
-    </div>
+      <>
+ sssss
+ <Header/>
+ <Nav/>
+ <Footer/>
+      </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import logo from './logo.png'
+function Header() {
+    return (
+    <header>
+      <h1>Little Lemon</h1>
+      <img src= {logo} alt="Little Lemon Logo" />
+    </header>
+    )
+}
+export default Header;
